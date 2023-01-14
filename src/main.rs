15c5b087a24fdcaceb08f200145b852f9fdd8d61
brandon phelps <bwp44f@mst.edu>
2023-01-13 20:43:50 -0600

    add state for can controller.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,56 @@
-use askama::Template;
-use axum::{Router, extract::{WebSocketUpgrade, ws::{Message, WebSocket}}, response::Response, routing::get};
-use futures_util::{SinkExt, StreamExt};
-use std::{collections::HashMap, net::SocketAddr};
+use std::sync::Arc;
 
-use tokio::net::{TcpListener, TcpStream};
+// this pulls in some of functions 
+use askama::Template;
+use axum::{Router, extract::{WebSocketUpgrade, ws::{Message, WebSocket}, State}, response::Response, routing::get, Extension};
+use tokio::sync::Mutex;
+
+
+
 
 // use futures_util::{future, StreamExt, TryStreamExt};
-
-
 #[derive(Template)]
 #[template(path = "home.html")]
 struct HomeTemplate {
 }
 
-
 async fn home() -> HomeTemplate {
     HomeTemplate { }
 }
 
-async fn handle_socket(mut socket: WebSocket) {
+enum ControllerError {
+}
+
+struct TestController {
+    
+}
+
+impl TestController {
+    pub fn up(&mut self) -> Result<(), ControllerError> {
+        println!("up");
+        Ok(())
+    }
+
+    pub fn down(&mut self) -> Result<(), ControllerError> {
+        println!("down");
+        Ok(())
+    }
+    pub fn left(&mut self) -> Result<(), ControllerError> {
+        println!("left");
+        Ok(())
+    }
+    pub fn right(&mut self) -> Result<(), ControllerError> {
+        println!("right");
+        Ok(())
+    }
+}
+
+
+async fn handle_socket(mut socket: WebSocket, state: Arc<Mutex<AppState>>) {
     while let Some(Ok(msg)) = socket.recv().await {
         if let Message::Text(msg) = msg {
+            state.lock().await.handle_message(&msg);
+
             if socket
                 .send(Message::Text(format!("You said: {msg}")))
                 .await
@@ -32,14 +62,28 @@ async fn handle_socket(mut socket: WebSocket) {
     }
 }
 
+struct AppState {
+    controller: TestController,
+}
 
-async fn websocket_test(ws: WebSocketUpgrade) -> Response {
-    ws.on_upgrade(handle_socket)
+impl AppState {
+    pub fn handle_message(&self, msg: &str) {
+        println!("AppState got a message: {}", msg);
+    }
+}
+
+
+async fn websocket_test(Extension(state): Extension<Arc<Mutex<AppState>>>, ws: WebSocketUpgrade) -> Response {
+    ws.on_upgrade(|socket| handle_socket(socket, state))
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tracing_subscriber::fmt()
+    let state = Arc::new(Mutex::new(AppState {
+        controller: TestController { }
+    }));
+ 
+   tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
         .init();
 
@@ -49,7 +93,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // tracing::subscriber::set_global_default(subscriber)?;
     let app = Router::new()
         .route("/", get(home))
-        .route("/ws", get(websocket_test));
+        .route("/ws", get(websocket_test))
+        .layer(Extension(state));
 
     axum::Server::bind(&"0.0.0.0:3000".parse()?)
         .serve(app.into_make_service())
